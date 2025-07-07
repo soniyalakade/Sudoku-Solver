7e19791682d90535e95de7186a3e1b1d163a4cdf
Soniya Lakade <soniyalakade@gmail.com.com>
2025-07-07 18:15:42 +0530

    Initial commit - Add Prodigy4.cpp (Sudoku Solver)

---
+++ Prodigy4.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+using namespace std;
+
+
+#define N 9
+
+void printGrid(int grid[N][N]){
+    cout<<"Solved Sudoku :\n";
+    for(int row=0; row<N; row++){
+        for(int col=0; col<N; col++){
+            cout<<grid[row][col]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+bool isSafe(int grid[N][N], int row, int col, int num){
+    for(int x=0; x<N; x++){
+        if(grid[row][x]==num||grid[x][col]==num)
+        return false;
+    }
+
+    int startRow=row-row%3;
+    int startCol=col-col%3;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(grid[i+startRow][j+startCol]==num){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool solveSudoku(int grid[N][N]){
+    for(int row=0; row<N; row++){
+        for(int col=0;col<N;col++){
+            if(grid[row][col]==0){
+                for(int num=1; num<=9; num++){
+                    if(isSafe(grid,row,col,num)){
+                        grid[row][col]=num;
+
+                        if(solveSudoku(grid))
+                            return true;
+                        
+                        grid[row][col]=0;
+                    }
+                } 
+            return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main(){
+    int grid[N][N]={
+        {3, 0, 6, 5, 0, 8, 4, 0, 0}, 
+      	{5, 2, 0, 0, 0, 0, 0, 0, 0}, 
+      	{0, 8, 7, 0, 0, 0, 0, 3, 1},
+        {0, 0, 3, 0, 1, 0, 0, 8, 0}, 
+      	{9, 0, 0, 8, 6, 3, 0, 0, 5}, 
+      	{0, 5, 0, 0, 9, 0, 6, 0, 0},
+        {1, 3, 0, 0, 0, 0, 2, 5, 0}, 
+      	{0, 0, 0, 0, 0, 0, 0, 7, 4}, 
+      	{0, 0, 5, 2, 0, 6, 3, 0, 0}
+    };
+
+    if(solveSudoku(grid))
+        printGrid(grid);
+    else
+        cout<<"No solution exits!"<<endl;
+    
+    return 0;
+}
